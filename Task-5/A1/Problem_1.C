161f0f0787539b9951f25082cad362f9ad5c3e82
Omar Attia <Omar.attia242@gmail.com>
2023-04-10 20:07:11 +0200

    Initialize the files

---
+++ Task-5/A1/Problem_1.C
@@ -0,0 +1,11 @@
+/*
+• Dynamic Memory Allocation:
+    o Create a C program that contains 3 different allocated
+    memory spaces using (malloc, calloc) and reallocate one of
+    them to a different space using(realloc) for each allocation
+    indicate if it was allocated successfully or not and then
+    comment and print the values that was assigned to these
+    memory blocks. ”Don’t forget to free your memory when you
+    finish ☺”
+*/
+
