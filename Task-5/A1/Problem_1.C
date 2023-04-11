3f4a46f93624f3b2550565c29dc1ad4600219c18
Omar Attia <Omar.attia242@gmail.com>
2023-04-11 05:15:29 +0200

    Finished p1

--- Task-5/A1/Problem_1.C
+++ Task-5/A1/Problem_1.C
@@ -8,4 +8,68 @@
     memory blocks. ”Don’t forget to free your memory when you
     finish ☺”
 */
+/*-----------------------------Notes----------------------------------------
+malloc is short name for "Memmory Allocation"
 
+*/
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int *ptr1, *ptr2, *ptr3;
+    int n1 = 5, n2 =4, n3 =3;
+
+    //allocate memory using malloc 
+    ptr1 = (int*)malloc(n1 *sizeof(int));
+    if (ptr1 == NULL){
+        printf("Memory allocation failed for pointer 1\n");
+    }
+    else{
+        printf("Memory allocation succeeded for pointer 1\n");
+    }
+    //allocate memory using callouc
+    ptr2 = (int*)calloc(n2, sizeof(int));
+    if (ptr2 == NULL){
+        printf("Memory allocation failed for Pointer 2\n");
+    }else{
+        printf("Memory allocation succeeded for Pointer 2\n");
+    }
+    //allocate memory using realloc
+    ptr3 = (int*)realloc(ptr1, n3* sizeof(int));
+    if (ptr3 == NULL){
+        printf("Memory allocation failed for Pointer 3\n");
+    }
+    else{
+        printf("Memory allocation succeeded for Pointer 3\n");
+    }
+    //--------------------------------------------------------------------
+    for (int i = 0; i <n1; i++){
+        ptr1[i] = i;
+    }
+    printf("Values assigned to ptr1:\n");
+    for (int i = 0; i <n1; i++)
+        printf("%d \t",ptr1[i]);
+    printf("\n");
+    //--------------------------------------------------------------------
+    for (int i = 0; i <n2; i++){
+        ptr2[i] = i+5;
+    }
+    //--------------------------------------------------------------------
+    printf("Values assigned to ptr2:\n");
+    for (int i = 0; i <n2; i++)
+        printf("%d \t",ptr2[i]);
+    printf("\n");
+    //--------------------------------------------------------------------
+    for (int i = 0; i <n3; i++){
+        ptr3[i] = i+10;
+    }
+    printf("Values assigned to ptr3:\n");
+    for (int i = 0; i <n3; i++)
+        printf("%d \t",ptr3[i]);
+    printf("\n");
+    //--------------------------------------------------------------------
+    //free allocated memory
+    free(ptr1);
+    free(ptr2);
+    free(ptr3);
+    return 0;
+}
