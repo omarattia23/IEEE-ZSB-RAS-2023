c747560e15f9901550bb663bde88cadeb33199f2
Omar Attia <Omar.attia242@gmail.com>
2023-04-30 17:06:15 +0300

    set_private and interface files

---
+++ DIO/DIO_interface.h
@@ -0,0 +1,19 @@
+/************************************************/
+/************* Name    : Omar Attia  ***********/
+/************* Date    : 27/8/2023  ***********/
+/************* SWC     : DIO      ************/
+/************* Version : 1.0    *************/
+/*******************************************/
+#ifndef DIO_INTERFACE_H
+#define DIO_INTERFACE_H
+
+
+u8 DIO_setPinDirectoin(u8 Copy_u8PortID, u8 Copy_u8PinID, u8 Copy_u8PinDirection);
+u8 DIO_setPinvALUE(u8 Copy_u8PortID, u8 Copy_PinId, u8 Copy_u8PinValue);
+u8 DIO_u8GetPinValue(u8 Copy_u8PortID, u8 Copy_PinID, u8 * Copy_pu8ReturnedPinValue);
+u8 DIO_u8SetPortDirection(u8 Copy_u8PortID, u8 Copy_u8PortDirection);
+u8 DIO_u8SetPortValue(u8 Copy_u8PortID, u8 Copy_u8PortValue);
+u8 DIO_u8GetPortValue(u8 Copy_u8PortID, u8 * Copy_pu8ReturnedPortValue);
+
+
+#endif
---
+++ DIO/DIO_private.h
@@ -0,0 +1,28 @@
+/************************************************/
+/************* Name    : Omar Attia  ***********/
+/************* Date    : 27/8/2023  ***********/
+/************* SWC     : DIO      ************/
+/************* Version : 1.0    *************/
+/*******************************************/
+#ifndef DIO_PRIVATE_H
+#define DIO_PRIVATE_H
+
+#define DIO_u8_PORTA_REG    *((u8 *)0x3B)
+#define DIO_u8_DDRA_REG     *((u8 *)0x3A)
+#define DIO_u8_PINA_REG     *((u8 *)0x39)
+
+#define DIO_u8_PORTB_REG    *((u8 *)0x38)
+#define DIO_u8_DDRB_REG     *((u8 *)0x37)
+#define DIO_u8_PINB_REG     *((u8 *)0x36)
+
+#define DIO_u8_PORTC_REG    *((u8 *)0x35)
+#define DIO_u8_DDRC_REG     *((u8 *)0x34)
+#define DIO_u8_PINC_REG     *((u8 *)0x33)
+
+#define DIO_u8_PORTD_REG    *((u8 *)0x32)
+#define DIO_u8_DDRD_REG     *((u8 *)0x31)
+#define DIO_u8_PIND_REG     *((u8 *)0x30)
+
+
+
+#endif
