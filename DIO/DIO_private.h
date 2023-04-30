ddddb0681013721bc27ab0f7a1adbd79ea269fe8
Omar Attia <Omar.attia242@gmail.com>
2023-04-30 17:13:13 +0300

    updated

--- DIO/DIO_private.h
+++ DIO/DIO_private.h
@@ -7,21 +7,21 @@
 #ifndef DIO_PRIVATE_H
 #define DIO_PRIVATE_H
 
-#define DIO_u8_PORTA_REG    *((u8 *)0x3B)
-#define DIO_u8_DDRA_REG     *((u8 *)0x3A)
-#define DIO_u8_PINA_REG     *((u8 *)0x39)
+#define DIO_u8_PORTA_REG    *((volatile u8 *)0x3B)
+#define DIO_u8_DDRA_REG     *((volatile u8 *)0x3A)
+#define DIO_u8_PINA_REG     *((volatile u8 *)0x39)
 
-#define DIO_u8_PORTB_REG    *((u8 *)0x38)
-#define DIO_u8_DDRB_REG     *((u8 *)0x37)
-#define DIO_u8_PINB_REG     *((u8 *)0x36)
+#define DIO_u8_PORTB_REG    *((volatile u8 *)0x38)
+#define DIO_u8_DDRB_REG     *((volatile u8 *)0x37)
+#define DIO_u8_PINB_REG     *((volatile u8 *)0x36)
 
-#define DIO_u8_PORTC_REG    *((u8 *)0x35)
-#define DIO_u8_DDRC_REG     *((u8 *)0x34)
-#define DIO_u8_PINC_REG     *((u8 *)0x33)
+#define DIO_u8_PORTC_REG    *((volatile u8 *)0x35)
+#define DIO_u8_DDRC_REG     *((volatile u8 *)0x34)
+#define DIO_u8_PINC_REG     *((volatile u8 *)0x33)
 
-#define DIO_u8_PORTD_REG    *((u8 *)0x32)
-#define DIO_u8_DDRD_REG     *((u8 *)0x31)
-#define DIO_u8_PIND_REG     *((u8 *)0x30)
+#define DIO_u8_PORTD_REG    *((volatile u8 *)0x32)
+#define DIO_u8_DDRD_REG     *((volatile u8 *)0x31)
+#define DIO_u8_PIND_REG     *((volatile u8 *)0x30)
 
 
 
