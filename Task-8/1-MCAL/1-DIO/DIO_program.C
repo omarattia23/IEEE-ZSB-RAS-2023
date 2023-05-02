856226bff5119396efd322789e0fe904d402ebce
Omar Attia <Omar.attia242@gmail.com>
2023-05-02 20:58:34 +0300

    update

--- Task-8/1-MCAL/1-DIO/DIO_program.C
+++ Task-8/1-MCAL/1-DIO/DIO_program.C
@@ -166,6 +166,116 @@ u8 DIO_u8GetPinValue(u8 Copy_u8PortID, u8 Copy_PinID, u8 * Copy_pu8ReturnedPinVa
         }
     }
 }
-u8 DIO_u8SetPortDirection(u8 Copy_u8PortID, u8 Copy_u8PortDirection);
-u8 DIO_u8SetPortValue(u8 Copy_u8PortID, u8 Copy_u8PortValue);
-u8 DIO_u8GetPortValue(u8 Copy_u8PortID, u8 * Copy_pu8ReturnedPortValue);
+u8 DIO_u8SetPortDirection(u8 Copy_u8PortID, u8 Copy_u8PortDirection){
+    u8 Local_u8ErrorState = STD_TYPES_OK;
+    if ((Copy_u8PortDirection == DIO_u8_OUTPUT) || (Copy_u8PortDirection == DIO_u8_INPUT))
+    {
+        switch (Copy_u8PortID)
+        {
+        case DIO_u8_PORTA:
+            switch (Copy_u8PortDirection)
+            {
+            case DIO_u8_OUTPUT:
+                DIO_u8_DDRA_REG = 0xff;
+                break;
+            case DIO_u8_INPUT:
+                DIO_u8_DDRA_REG = 0x00;
+                break;
+            }
+            break;
+            case DIO_u8_PORTB:
+                switch (Copy_u8PortDirection)
+                {
+                case DIO_u8_OUTPUT:
+                    DIO_u8_DDRB_REG = 0xff;
+                    break;
+                case DIO_u8_INPUT:
+                    DIO_u8_DDRB_REG = 0x00;
+                    break;
+                }
+                break;
+            case DIO_u8_PORTC:
+                switch (Copy_u8PortDirection)
+                {
+                case DIO_u8_OUTPUT:
+                    DIO_u8_DDRC_REG = 0xff;
+                    break;
+                case DIO_u8_INPUT:
+                    DIO_u8_DDRC_REG = 0x00;
+                    break;
+                }
+                break;
+            case DIO_u8_PORTD:
+                switch (Copy_u8PortDirection)
+                {
+                case DIO_u8_OUTPUT:
+                    DIO_u8_DDRD_REG = 0xff;
+                    break;
+                case DIO_u8_INPUT:
+                    DIO_u8_DDRD_REG = 0x00;
+                    break;
+                }
+                break;
+            default: Local_u8ErrorState =STD_TYPES_NOK;
+    
+        }
+    }
+    else
+    {
+        Local_u8ErrorState = STD_TYPES_NOK;
+    }
+    
+    return Local_u8ErrorState
+}
+u8 DIO_u8SetPortValue(u8 Copy_u8PortID, u8 Copy_u8PortValue){
+    u8 Local_u8ErrorState = STD_TYPES_OK;
+    switch (Copy_u8PortID)
+    {
+    case DIO_u8_PORTA:
+        DIO_u8_PORTA_REG =Copy_u8PortValue;
+        break;
+    case DIO_u8_PORTB:
+        DIO_u8_PORTB_REG =Copy_u8PortValue;
+        break;
+    case DIO_u8_PORTC:
+        DIO_u8_PORTC_REG =Copy_u8PortValue;
+        break;
+    case DIO_u8_PORTD:
+        DIO_u8_PORTD_REG =Copy_u8PortValue;
+        break;
+    
+    default: Local_u8ErrorState =STD_TYPES_NOK;
+        break;
+    }
+    return Local_u8ErrorState;
+}
+u8 DIO_u8GetPortValue(u8 Copy_u8PortID, u8 * Copy_pu8ReturnedPortValue){
+    u8 Local_u8ErrorState = STD_TYPES_OK;
+    if (Copy_pu8ReturnedPortValue != NULL)
+    {
+        switch (Copy_u8PortId)
+        {
+        case DIO_u8_PORTA:
+           *Copy_pu8ReturnedPortValue =DIO_u8_PINA_REG;
+            break;
+        case DIO_u8_PORTB:
+           *Copy_pu8ReturnedPortValue =DIO_u8_PINB_REG;
+            break;
+        case DIO_u8_PORTC:
+           *Copy_pu8ReturnedPortValue =DIO_u8_PINC_REG;
+            break;
+        case DIO_u8_PORTD:
+           *Copy_pu8ReturnedPortValue =DIO_u8_PIND_REG;
+            break;
+        
+        default: Local_u8ErrorState =STD_TYPES_NOK;
+            break;
+        }
+    }
+    else
+    {
+        Local_u8ErrorState = STD_TYPES_NOK;
+    }
+    
+    return Local_u8ErrorState;
+}
--- Task-8/1-MCAL/1-DIO/main.C
+++ Task-8/1-MCAL/1-DIO/main.C
@@ -13,6 +13,6 @@ int main(){
         _delay_ms(500);
         DIO_u8SetPinValue(DIO_u8_PORTC,DIO_u8_PIN7,DIO_u8_LOW);
         _delay_ms(500);
-    
+    }
     return 0;
 }
