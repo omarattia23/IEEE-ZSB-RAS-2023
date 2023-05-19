7416914d3a36917889c1a06d26051fa5dcd4e85e
Omar Attia <Omar.attia242@gmail.com>
2023-05-19 23:12:10 +0300

    update

--- Task-10/SpecialCharacters/APP/main.c
+++ Task-10/SpecialCharacters/APP/main.c
@@ -3,14 +3,14 @@
 #include <string.h>
 #include <util/delay.h>
 
-// u8 Character1[8] = { 0x01, 0x03, 0x07, 0x0D, 0x0F, 0x02, 0x05, 0x0A };
-// u8 Character2[8] = { 0x10, 0x18, 0x1C, 0x16, 0x1E, 0x08, 0x14, 0x0A };
-// u8 Character3[8] = { 0x01, 0x03, 0x07, 0x0D, 0x0F, 0x05, 0x08, 0x04 };
-// u8 Character4[8] = { 0x10, 0x18, 0x1C, 0x16, 0x1E, 0x14, 0x02, 0x04 };
-// u8 Character5[8] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x18, 0x18 };
-// u8 Character6[8] = { 0x00, 0x00, 0x00, 0x00, 0x06, 0x06, 0x00, 0x00 };
-// u8 Character7[8] = { 0x00, 0x00, 0x03, 0x03, 0x00, 0x00, 0x00, 0x00 };
-// u8 Character8[8] = {0x00, 0x18, 0x18, 0x00, 0x00, 0x00, 0x00, 0x00};
+//u8 Character1[8] = { 0x01, 0x03, 0x07, 0x0D, 0x0F, 0x02, 0x05, 0x0A };
+//u8 Character2[8] = { 0x10, 0x18, 0x1C, 0x16, 0x1E, 0x08, 0x14, 0x0A };
+//u8 Character3[8] = { 0x01, 0x03, 0x07, 0x0D, 0x0F, 0x05, 0x08, 0x04 };
+//u8 Character4[8] = { 0x10, 0x18, 0x1C, 0x16, 0x1E, 0x14, 0x02, 0x04 };
+//u8 Character5[8] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x18, 0x18 };
+//u8 Character6[8] = { 0x00, 0x00, 0x00, 0x00, 0x06, 0x06, 0x00, 0x00 };
+//u8 Character7[8] = { 0x00, 0x00, 0x03, 0x03, 0x00, 0x00, 0x00, 0x00 };
+//u8 Character8[8] = {0x00, 0x18, 0x18, 0x00, 0x00, 0x00, 0x00, 0x00};
 u8 Character1[8] = {0x0e, 0x0e, 0x04, 0x04, 0x1f, 0x04, 0x0a, 0x0a};
 u8 Character2[8] = {0b00011111, 0b00001110, 0b00011111, 0b00000100, 0b00001110, 0b00011111, 0b00000100, 0b00000100};
 u8 Character3[8] = {0x04, 0x0E, 0x0E, 0x0E, 0x1F, 0x00, 0x04, 0x00};
@@ -48,13 +48,13 @@ int main() {
 	u8 button7_pin = DIO_u8_PIN6;
 
 	// Example button states (modify based on your setup)
-	u8 button1_state;
-	u8 button2_state;
-	u8 button3_state;
-	u8 button4_state;
-	u8 button5_state;
-	u8 button6_state;
-	u8 button7_state;
+	u8 button1_state = DIO_u8_LOW;
+	u8 button2_state = DIO_u8_LOW;
+	u8 button3_state = DIO_u8_LOW;
+	u8 button4_state = DIO_u8_LOW;
+	u8 button5_state = DIO_u8_LOW;
+	u8 button6_state = DIO_u8_LOW;
+	u8 button7_state = DIO_u8_LOW;
 
 	// Example button special character mappings
 	u8 *button1_special_char = Character1;
@@ -74,63 +74,64 @@ int main() {
 		button3_state = DIO_u8GetPinValue(DIO_u8_PORTB, button3_pin,
 				&button3_state);
 		button4_state = DIO_u8GetPinValue(DIO_u8_PORTB, button4_pin,
-				&button3_state);
+				&button4_state);
 		button5_state = DIO_u8GetPinValue(DIO_u8_PORTB, button5_pin,
-				&button3_state);
+				&button5_state);
 		button6_state = DIO_u8GetPinValue(DIO_u8_PORTB, button6_pin,
-				&button3_state);
+				&button6_state);
 		button7_state = DIO_u8GetPinValue(DIO_u8_PORTB, button7_pin,
-				&button3_state);
+				&button7_state);
 
 		// Button 1
-		if (button1_state) {
+		if (button1_state == DIO_u8_HIGH) {
 			LCD_voidSendSpecialCharacters(0, button1_special_char, LCD_u8_LINE2,
-					count + 0, 0b01000000);
+					count + 0, 0x40);
+			_delay_ms(200);
 		}
 
 		// Button 2
-		if (button2_state) {
+		else if (button2_state == DIO_u8_HIGH) {
 			LCD_voidSendSpecialCharacters(1, button2_special_char, LCD_u8_LINE2,
-					count + 2, 0x48);
+					count + 1, 0x50);
 					// Add a small delay to avoid unnecessary processing
-		_delay_ms(1000);
+		_delay_ms(200);
 		}
 
 		// Button 3
-		if (button3_state) {
+		else if (button3_state == DIO_u8_HIGH) {
 			LCD_voidSendSpecialCharacters(2, button3_special_char, LCD_u8_LINE2,
-					count + 4, 0x50);
+					count + 2, 0x58);
 					// Add a small delay to avoid unnecessary processing
-		_delay_ms(1000);
+		_delay_ms(200);
 		}
 		// Button 4
-		if (button4_state) {
+		else if (button4_state == DIO_u8_HIGH) {
 			LCD_voidSendSpecialCharacters(2, button4_special_char, LCD_u8_LINE2,
-					count + 6, 0x50);
+					count + 6, 0x60);
 					// Add a small delay to avoid unnecessary processing
-		_delay_ms(1000);
+		_delay_ms(200);
 		}
 		// Button 5
-		if (button5_state) {
+		else if (button5_state == DIO_u8_HIGH) {
 			LCD_voidSendSpecialCharacters(2, button5_special_char, LCD_u8_LINE2,
-					count + 8, 0x50);
+					count + 8, 0x68);
 					// Add a small delay to avoid unnecessary processing
-		_delay_ms(1000);
+		_delay_ms(200);
 		}
 		// Button 6
-		if (button6_state) {
+		else if (button6_state == DIO_u8_HIGH) {
 			LCD_voidSendSpecialCharacters(2, button6_special_char, LCD_u8_LINE2,
-					count + 10, 0x50);
+					count + 10, 0x70);
 					// Add a small delay to avoid unnecessary processing
-		_delay_ms(1000);
+		_delay_ms(200);
 		}
 		// Button 7
-		if (button7_state) {
+		else if (button7_state == DIO_u8_HIGH) {
 			LCD_voidSendSpecialCharacters(2, button7_special_char, LCD_u8_LINE2,
-					count + 12, 0x50);
+					count + 12, 0x78);
 		}
 
 		// Add a small delay to avoid unnecessary processing
-		_delay_ms(1000);
+		_delay_ms(200);
 	}
 }
