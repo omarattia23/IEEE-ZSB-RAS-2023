dc203197c46aa0fd461ed8a2dc989a73f83e2ba1
Omar Attia <Omar.attia242@gmail.com>
2023-07-27 22:03:16 +0300

    update stepper motor

--- Task-14/Motor/HAL/4-stepper_motor/stepper_program.c
+++ Task-14/Motor/HAL/4-stepper_motor/stepper_program.c
@@ -21,7 +21,7 @@ void StepperMotor_move(u8 direction, u16 angle)
     DIO_voidSetPinDirection(stepper_motor_port, stepper_motor_orange, DIO_u8_OUTPUT);
 
     // Calculate the number of steps based on the motor's step angle and microstepping settings
-    u16 steps = (angle * STEPS_PER_REVOLUTION) / 360;
+    u16 steps = ((angle * STEPS_PER_REVOLUTION) / 360) / 4;
 
     if (direction == CW)
     {
@@ -30,8 +30,23 @@ void StepperMotor_move(u8 direction, u16 angle)
             // Define the step sequence for clockwise rotation
             DIO_voidSetPinValue(stepper_motor_port, stepper_motor_blue, DIO_u8_HIGH);
             DIO_voidSetPinValue(stepper_motor_port, stepper_motor_pink, DIO_u8_LOW);
+            DIO_voidSetPinValue(stepper_motor_port, stepper_motor_yellow, DIO_u8_LOW);
+            DIO_voidSetPinValue(stepper_motor_port, stepper_motor_orange, DIO_u8_LOW);
+            // Define the step sequence for clockwise rotation
+            DIO_voidSetPinValue(stepper_motor_port, stepper_motor_blue, DIO_u8_LOW);
+            DIO_voidSetPinValue(stepper_motor_port, stepper_motor_pink, DIO_u8_HIGH);
+            DIO_voidSetPinValue(stepper_motor_port, stepper_motor_yellow, DIO_u8_LOW);
+            DIO_voidSetPinValue(stepper_motor_port, stepper_motor_orange, DIO_u8_LOW);
+            // Define the step sequence for clockwise rotation
+            DIO_voidSetPinValue(stepper_motor_port, stepper_motor_blue, DIO_u8_LOW);
+            DIO_voidSetPinValue(stepper_motor_port, stepper_motor_pink, DIO_u8_LOW);
             DIO_voidSetPinValue(stepper_motor_port, stepper_motor_yellow, DIO_u8_HIGH);
             DIO_voidSetPinValue(stepper_motor_port, stepper_motor_orange, DIO_u8_LOW);
+            // Define the step sequence for clockwise rotation
+            DIO_voidSetPinValue(stepper_motor_port, stepper_motor_blue, DIO_u8_LOW);
+            DIO_voidSetPinValue(stepper_motor_port, stepper_motor_pink, DIO_u8_LOW);
+            DIO_voidSetPinValue(stepper_motor_port, stepper_motor_yellow, DIO_u8_LOW);
+            DIO_voidSetPinValue(stepper_motor_port, stepper_motor_orange, DIO_u8_HIGH);
 
             _delay_ms(StepperDelay); // Add a delay to control the motor speed
         }
@@ -42,9 +57,24 @@ void StepperMotor_move(u8 direction, u16 angle)
         {
             // Define the step sequence for counterclockwise rotation
             DIO_voidSetPinValue(stepper_motor_port, stepper_motor_blue, DIO_u8_LOW);
-            DIO_voidSetPinValue(stepper_motor_port, stepper_motor_pink, DIO_u8_HIGH);
+            DIO_voidSetPinValue(stepper_motor_port, stepper_motor_pink, DIO_u8_LOW);
             DIO_voidSetPinValue(stepper_motor_port, stepper_motor_yellow, DIO_u8_LOW);
             DIO_voidSetPinValue(stepper_motor_port, stepper_motor_orange, DIO_u8_HIGH);
+            // Define the step sequence for counterclockwise rotation
+            DIO_voidSetPinValue(stepper_motor_port, stepper_motor_blue, DIO_u8_LOW);
+            DIO_voidSetPinValue(stepper_motor_port, stepper_motor_pink, DIO_u8_LOW);
+            DIO_voidSetPinValue(stepper_motor_port, stepper_motor_yellow, DIO_u8_HIGH);
+            DIO_voidSetPinValue(stepper_motor_port, stepper_motor_orange, DIO_u8_LOW);
+            // Define the step sequence for counterclockwise rotation
+            DIO_voidSetPinValue(stepper_motor_port, stepper_motor_blue, DIO_u8_LOW);
+            DIO_voidSetPinValue(stepper_motor_port, stepper_motor_pink, DIO_u8_HIGH);
+            DIO_voidSetPinValue(stepper_motor_port, stepper_motor_yellow, DIO_u8_LOW);
+            DIO_voidSetPinValue(stepper_motor_port, stepper_motor_orange, DIO_u8_LOW);
+            // Define the step sequence for counterclockwise rotation
+            DIO_voidSetPinValue(stepper_motor_port, stepper_motor_blue, DIO_u8_HIGH);
+            DIO_voidSetPinValue(stepper_motor_port, stepper_motor_pink, DIO_u8_LOW);
+            DIO_voidSetPinValue(stepper_motor_port, stepper_motor_yellow, DIO_u8_LOW);
+            DIO_voidSetPinValue(stepper_motor_port, stepper_motor_orange, DIO_u8_LOW);
 
             _delay_ms(StepperDelay); // Add a delay to control the motor speed
         }
