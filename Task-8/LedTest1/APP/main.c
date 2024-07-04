6f99ba418c3f0200ee3bf59318f05953e9c875cc
unknown <omar.attia242@gmail.com>
2024-07-04 03:30:29 +0300

    optimize delay function

--- Task-8/LedTest1/APP/main.c
+++ Task-8/LedTest1/APP/main.c
@@ -1,26 +1,20 @@
 #include "../MCAL/DIO/DIO_interface.h"
-
-void delay(void)
-	{
-		for(int x=0;x<500;x++)
-			for(int y=0;y<500;y++)
-				__asm("NOP");
-	}
-int main()
-
+#define CPU 1000000
+void delay(int time)
 {
-
+	int cylces = (time * CPU) / 1000;
+	for (int x = 0; x < cylces; x++)
+		__asm("NOP");
+}
+int main()
+{
 	DIO_voidInit();
-			while(1)
-			{
-
-				DIO_voidSetPinValue(DIO_u8_PORTA,DIO_u8_PIN0,DIO_u8_HIGH);
-				delay();
-				DIO_voidSetPinValue(DIO_u8_PORTB,DIO_u8_PIN1,DIO_u8_LOW);
-
-				delay();
-
-			}
-		return 0;
-
+	while (1)
+	{
+		DIO_voidSetPinValue(DIO_u8_PORTA, DIO_u8_PIN0, DIO_u8_HIGH);
+		delay(1000);
+		DIO_voidSetPinValue(DIO_u8_PORTB, DIO_u8_PIN1, DIO_u8_LOW);
+		delay(1000);
+	}
+	return 0;
 }
